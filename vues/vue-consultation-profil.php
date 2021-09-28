b400ffe33c04c210368e957950e837c495cdc839
AntoineTSIO <antoine.touzart.sio@gmail.com>
2021-09-28 12:52:33 +0200

    V1.1.1

---
+++ vues/vue-consultation-profil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Profil</h1>
+    <?php
+
+
+        echo $_SESSION[ 'numero' ];
+    	echo $_SESSION[ 'nom' ];
+		echo $_SESSION[ 'prenom' ] ;
+
+        
+    ?>
+</body>
+
+</html>
