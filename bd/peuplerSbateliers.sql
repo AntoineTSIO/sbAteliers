0c76874881085a8e825cf1b0855f75bb18de3d1a
AntoineTSIO <antoine.touzart.sio@gmail.com>
2021-12-07 12:58:30 +0100

    V1.2.5

--- bd/peuplerSbateliers.sql
+++ bd/peuplerSbateliers.sql
@@ -1,26 +1,26 @@
 USE sbateliers ;
-
+ 
 INSERT INTO Client (numero, civilite, nom, prenom, date_de_naissance, adresse_electronique, mot_de_passe, adresse_postale, code_postal, ville, numero_de_telephone) VALUES 
-(0,'Monsieur','TEST','test','2000-01-01','test@test.fr','azerty','55 Rue du Faubourg Saint-Honoré','75008','Paris','0601020304'),
-(1,'Monsieur','LAGRANDE','Gaspar','1989-08-14','GasparLaGrande@jourrapide.com','azerty','7 Rue Marie de Médicis','59400','Cambrai','03.33.02.21.37'),
-(2,'Madame','COUPART','Yolande','1952-09-15','YolandeCoupart@teleworm.us','azerty','81 Chemin Du Lavarin Sud','94230','Cachan','01.65.70.43.39'),
-(3,'Madame','VOISINE','Susanne','1988-02-02','SusanneVoisine@dayrep.com','azerty','43 rue Gontier-Patin','47000','Agen','05.63.85.75.48'),
-(4,'Monsieur','RICHARD','Clovis','1959-08-17','ClovisRichard@dayrep.com','azerty','45 Rue du Général Ailleret','97139','Les Abymes','05.32.36.84.49');
-
+(0,'Monsieur',AES_ENCRYPT('TEST',SHA2('password',512)),AES_ENCRYPT('test',SHA2('password',512)),AES_ENCRYPT('2000-01-01',SHA2('password',512)),AES_ENCRYPT('test@test;fr',SHA2('password',512)),'$2y$10$iajv3F9u.YZV5HZIGUVoxOFg7SDv3Ltv1iWNAmh0.6N/YYIi4TJyO',AES_ENCRYPT('55 Rue du Faubourg Saint-Honoré',SHA2('password',512)),'75008','Paris',AES_ENCRYPT('01.02.03.04.05',SHA2('password',512))),
+(1,'Monsieur',AES_ENCRYPT('LAGRANDE',SHA2('password',512)),AES_ENCRYPT('Gaspar',SHA2('password',512)),AES_ENCRYPT('1989-08-14',SHA2('password',512)),AES_ENCRYPT('GasparLaGrande@jourrapide.com',SHA2('password',512)),'$2y$10$eBu4zL8n8CyKBZYZaK9UAeLzGHQ07kRZI54Gns.wwnwd6kjSGwvpu',AES_ENCRYPT('7 Rue Marie de Médicis',SHA2('password',512)),'59400','Cambrai',AES_ENCRYPT('03.33.02.21.37',SHA2('password',512))),
+(2,'Madame',AES_ENCRYPT('COUPART',SHA2('password',512)),AES_ENCRYPT('Yolande',SHA2('password',512)),AES_ENCRYPT('1952-09-15',SHA2('password',512)),AES_ENCRYPT('YolandeCoupart@teleworm.us',SHA2('password',512)),'$2y$10$vLDOPgsRjX2ydklF58pyKOxjO/15AYWJxtPcnTUHdu2eBWUfFWmmq',AES_ENCRYPT('81 Chemin du Lavarin Sud',SHA2('password',512)),'94230','Cachan',AES_ENCRYPT('01.65.70.43.39',SHA2('password',512))),
+(3,'Madame',AES_ENCRYPT('VOISINE',SHA2('password',512)),AES_ENCRYPT('Susanne',SHA2('password',512)),AES_ENCRYPT('1988-02-02',SHA2('password',512)),AES_ENCRYPT('SusanneVoisine@dayrep.com',SHA2('password',512)),'$2y$10$k4ixWOhefuMrOZsZg1f8ieKRIF6sn2IhTHow4JekGO4P6ZOAYaGbe',AES_ENCRYPT('43 Rue Gontier-Patin',SHA2('password',512)),'47000','Agen',AES_ENCRYPT('05.63.85.75.48',SHA2('password',512))),
+(4,'Monsieur',AES_ENCRYPT('RICHARD',SHA2('password',512)),AES_ENCRYPT('Clovis',SHA2('password',512)),AES_ENCRYPT('1959-08-17',SHA2('password',512)),AES_ENCRYPT('ClovisRichard@dayrep.com',SHA2('password',512)),'$2y$10$Dn0KxnoEDjROG5RykNAPCOpv387Uk7mskvJNf/o0oVC8v7W1bByNW',AES_ENCRYPT('45 Rue du Général Ailleret',SHA2('password',512)),'97139','Les Abymes',AES_ENCRYPT('05.32.36.84.49',SHA2('password',512)));
+ 
 INSERT INTO Responsable_Ateliers (numero, nom_de_connexion, nom, prenom, mot_de_passe) VALUES 
-(0,'usertest','TEST','test','azerty1234'),
-(1,'anolan','ALMOSNI','Nolan','azerty'),
-(2,'gclement','GALVEZ','Clement','azerty'), 
-(3,'dshugo','DA SILVA','Hugo','azerty'),
-(4,'rstella','RICHAL','Stella','azerty'), 
-(5,'jtheo','JEAN','Theo','azerty');
-
+(0,'usertest',AES_ENCRYPT('TEST',SHA2('password',512)),AES_ENCRYPT('test',SHA2('password',512)),'$2y$10$DL9xdDZ4RQFyAabZ3PEOCuJpENKWHPKJZ4pWBsosmPlkt.wrzmU6u'),
+(1,'urudolph',AES_ENCRYPT('UNDERHILL',SHA2('password',512)),AES_ENCRYPT('Rudolph',SHA2('password',512)),'$2y$10$CGc0jDaZIfgox4uJfUttL.vIGHOt2EuW4kzxc20t58PuE4cDjMtSy'),
+(2,'sgruffo',AES_ENCRYPT('SANDYMAN',SHA2('password',512)),AES_ENCRYPT('Gruffo',SHA2('password',512)),'$2y$10$pgfYRrYTUnLcpTPwlcyJnuSIFvYU/St587c0BWyxrzPV3xMrfNPHi'), 
+(3,'bdiamond',AES_ENCRYPT('BAGGINS',SHA2('password',512)),AES_ENCRYPT('Diamond',SHA2('password',512)),'$2y$10$MaW.yhPtdNM1pScbdjV8NeP5d8ad38y76pk9grBEyA3k8GSwtzKSy'),
+(4,'bprisca',AES_ENCRYPT('BUTTON',SHA2('password',512)),AES_ENCRYPT('Prisca',SHA2('password',512)),'$2y$10$nr2wEujdaz7.6/AwMyhyhO5TWGRfaeCx3Xg8jcxWgTsmbzrmwncfG'), 
+(5,'bmenegilda',AES_ENCRYPT('BANKS',SHA2('password',512)),AES_ENCRYPT('Menegilda',SHA2('password',512)),'$2y$10$MiPeavVCnwJV2POsOD.fcO80X4Z42p9yyouA/WhQwmskhFq65JuUy');
+ 
 INSERT INTO Atelier (numero, responsable, date_enregistrement, date_et_heure_programmees, duree, nombre_de_places, theme) VALUES
 (1, 1, '2021-10-10', '2021-10-17 09:00:00', '00:30:00', 10, "Accueil des nouveaux clients"),
 (2, 2, '2021-10-11', '2021-10-18 09:00:00', '00:45:00', 20, "Explications sur l'initiative de Sanaya Bio"),
 (3, 5, '2021-10-12', '2021-10-19 09:00:00', '01:30:00', 5, "Conception de Parfums");
-
-INSERT INTO Participation (date_inscription, client, atelier) VALUES
+ 
+INSERT INTO Participer (date_inscription, client, atelier) VALUES
 ('2021-10-15',1,1),
 ('2021-10-15',2,1),
 ('2021-10-16',3,2),
@@ -30,4 +30,3 @@ INSERT INTO Participation (date_inscription, client, atelier) VALUES
 ('2021-10-18',3,3),
 ('2021-10-18',4,3),
 ('2021-10-18',1,3);
-
--- vues/vue-connexion.php
+++ vues/vue-connexion.php
@@ -2,13 +2,48 @@
 <html lang="fr">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="../style/bulma.css">
+    <link rel="stylesheet" type="text/css" href="../style/fontawesome.css">
+    <script src="https://kit.fontawesome.com/a5be86d7bf.js" crossorigin="anonymous"></script>
     <meta charset="utf-8">
 	<title>Sanaya Bio Ateliers - Connexion</title>
 </head>
 
 <body>
+
+<section class="hero is-primary is-fullheight">
+  <div class="hero-body">
+    <div class="">
+      <p class="title">
+          Sanaya Bio Ateliers
+      </p>
+      <p class="subtitle">
+          Se Connecter
+      </p>
+      <div class="field">
+        <label for="mail">Adresse Electronique :</label>
+        <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="mail@example.com">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+            </span>
+        </p>
+      </div>
+      <div class="field">
+        <label for="mdp">Mot de Passe :</label>
+        <p class="control has-icons-left">
+            <input class="input" type="password">
+            <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+            </span>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <div class="col-sm">
     <h1 class="text-center">Se Connecter</h1>
@@ -35,6 +70,7 @@
         </div>
     </form>
 </div>
+
 
 
 </body>
